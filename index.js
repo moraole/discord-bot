e67039feb448077271e553c442da8232b5e93f53
moraole <moraole@wwu.edu>
2021-04-06 19:40:29 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ const {
 } = require('./config.json');
 const inosuke = new Discord.Client();
 let chrome = require ('selenium-webdriver/chrome');
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const width = 3000
-const height = 2000
-const webdriver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless().windowSize({ width, height})).build();
-const fs = require('fs')
+const { Builder, By, Key, until, promise, Actions,} = require('selenium-webdriver');
+var axis = 20;
+const webdriver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments("--window-size=1920,1080")).build();
+const fs = require('fs');
 const queue = new Map();
-const ytdl = require('ytdl-core');
-
 
 inosuke.once("ready", () => {
+  
   console.log("Ready!");
 });
 
@@ -55,12 +53,16 @@ inosuke.on("message", async message => {
 
 async function chegg(message) {
   const args = message.content.split(" ");
+  const body = (await webdriver).findElement(By.xpath('/html/body'));
+  console.log(body);
   if (args[1] == `help`) {
     return message.channel.send(`
       List of current commands: 
       chegg help - This command is working.
       chegg help <command> NOT WORKING YET.
       chegg link <link> - In development...
+      chegg bypass - bypass captcha when shown.
+      chegg next (Might be temporary)
       chegg options - NOT WORKING YET.
       chegg enable - NOT WORKING YET.
       chegg reset - NOT WORKING YET.
@@ -68,103 +70,43 @@ async function chegg(message) {
   } else if (args[1] == `link`) {
     let cheggLink = args[2];
     await webdriver.get(cheggLink);
-    webdriver.wait(until.elementLocated(By.name('answers-h2')), 5 * 1000);
+    webdriver.wait(until.elementLocated(By.xpath('/html/body')), 5 * 1000);
     
     await webdriver.takeScreenshot().then(function (data) {
       fs.writeFileSync('./images/img.png', data, 'base64');
     });
     message.channel.send("", { files: ["./images/img.png"] });
-  } else {
+  }else if (args[1] == `next`) {
+    body.sendKeys(Key.PAGE_DOWN);
+    webdriver.wait(until.elementLocated(By.xpath('/html/body')), 5 * 1000);
+    await webdriver.takeScreenshot().then(function (data) {
+      fs.writeFileSync('./images/img.png', data, 'base64');
+    });
+    message.channel.send("", { files: ["./images/img.png"] });
+  } else if (args[1] == `bypass`) {
+    
+  }
+  else {
     return message.channel.send(args[1] + " Command is incomplete or not a valid chegg command.");
   }
 }
-
-async function execute(message, serverQueue) {
-  const args = message.content.split(" ");
-
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel)
-    return message.channel.send(
-      "You need to be in a voice channel to play music!"
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user);
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-    return message.channel.send(
-      "I need the permissions to join and speak in your voice channel!"
-    );
-  }
-
-  const songInfo = await ytdl.getInfo(args[1]);
-  const song = {
-    title: songInfo.title,
-    url: songInfo.video_url
-  };
-
-  if (!serverQueue) {
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true
-    };
-
-    queue.set(message.guild.id, queueContruct);
-
-    queueContruct.songs.push(song);
-
-    try {
-      var connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      console.log(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    return message.channel.send(`${song.title} has been added to the queue!`);
-  }
+/**
+ * Returns list of commands needing some work to be done.
+ *
+ * @param {string} 
+ * @return {string} message.channel.send() a message with things to do to get the bot fully functioning.
+ */
+ function toDo(message) {
+  return message.channel.send(
+  `   fix all async functions (including toDo) using **module.exports**
+      ** Answer only display **
+      Final step: Remove elements and print results!
+      - Add other commands
+      * Implement everything else in -chegg help command.
+      - Figure out how to make bot DM Users.
+      * DM users chegg images to avoid spam in channels. Another option would be to
+      constantly clear chat using bot.
+  `)
 }
 
-function skip(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    );
-  if (!serverQueue)
-    return message.channel.send("There is no song that I could skip!");
-  serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-  if (!message.member.voice.channel)
-    return message.channel.send(
-      "You have to be in a voice channel to stop the music!"
-    );
-  serverQueue.songs = [];
-  serverQueue.connection.dispatcher.end();
-
-}
-
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id);
-  if (!song) {
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
-
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-      serverQueue.songs.shift();
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
 inosuke.login(token);
