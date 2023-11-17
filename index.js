261b685cfaf4a019e81e3cde4906ed2fae2ab03d
Erick Mora <77127047+moraole@users.noreply.github.com>
2023-11-17 14:26:32 -0800

    Update index.js to be generic

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
   prefix,
   token,
 } = require('./config.json');
-const inosuke = new Discord.Client();
+const DBot = new Discord.Client();
 let chrome = require ('selenium-webdriver/chrome');
 const { Builder, By, Key, until, promise, Actions,} = require('selenium-webdriver');
 var axis = 20;
@@ -13,20 +13,20 @@ const webdriver = new Builder().forBrowser('chrome').setChromeOptions(new chrome
 const fs = require('fs');
 const queue = new Map();
 
-inosuke.once("ready", () => {
+DBot.once("ready", () => {
   
   console.log("Ready!");
 });
 
-inosuke.once("reconnecting", () => {
+DBot.once("reconnecting", () => {
   console.log("Reconnecting!");
 });
 
-inosuke.once("disconnect", () => {
+DBot.once("disconnect", () => {
   console.log("Disconnect!");
 });
 
-inosuke.on("message", async message => {
+DBot.on("message", async message => {
 
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
@@ -43,33 +43,33 @@ inosuke.on("message", async message => {
   } else if (message.content.startsWith(`${prefix}stop`)) {
     stop(message, serverQueue);
     return;
-  } else if (message.content.startsWith(`${prefix}chegg`) || message.content.startsWith(`${prefix}c`)) {
-    chegg(message);
+  } else if (message.content.startsWith(`${prefix}DBot`) || message.content.startsWith(`${prefix}c`)) {
+    DBot(message);
   }
   else {
     message.channel.send("You need to enter a valid command!");
   }
 });
 
-async function chegg(message) {
+async function DBot(message) {
   const args = message.content.split(" ");
   const body = (await webdriver).findElement(By.xpath('/html/body'));
   console.log(body);
   if (args[1] == `help`) {
     return message.channel.send(`
       List of current commands: 
-      chegg help - This command is working.
-      chegg help <command> NOT WORKING YET.
-      chegg link <link> - In development...
-      chegg bypass - bypass captcha when shown.
-      chegg next (Might be temporary)
-      chegg options - NOT WORKING YET.
-      chegg enable - NOT WORKING YET.
-      chegg reset - NOT WORKING YET.
-      chegg error <error message> - Not a command yet, Send errors or problems to Erick Mora :D`);
+      DBot help - This command is working.
+      DBot help <command> NOT WORKING YET.
+      DBot link <link> - In development...
+      DBot bypass - bypass captcha when shown.
+      DBot next (Might be temporary)
+      DBot options - NOT WORKING YET.
+      DBot enable - NOT WORKING YET.
+      DBot reset - NOT WORKING YET.
+      DBot error <error message> - Not a command yet, Send errors or problems to Erick Mora :D`);
   } else if (args[1] == `link`) {
-    let cheggLink = args[2];
-    await webdriver.get(cheggLink);
+    let DBotLink = args[2];
+    await webdriver.get(DBotLink);
     webdriver.wait(until.elementLocated(By.xpath('/html/body')), 5 * 1000);
     
     await webdriver.takeScreenshot().then(function (data) {
@@ -87,7 +87,7 @@ async function chegg(message) {
     
   }
   else {
-    return message.channel.send(args[1] + " Command is incomplete or not a valid chegg command.");
+    return message.channel.send(args[1] + " Command is incomplete or not a valid DBot command.");
   }
 }
 /**
@@ -102,11 +102,11 @@ async function chegg(message) {
       ** Answer only display **
       Final step: Remove elements and print results!
       - Add other commands
-      * Implement everything else in -chegg help command.
+      * Implement everything else in -DBot help command.
       - Figure out how to make bot DM Users.
-      * DM users chegg images to avoid spam in channels. Another option would be to
+      * DM users DBot images to avoid spam in channels. Another option would be to
       constantly clear chat using bot.
   `)
 }
 
-inosuke.login(token);
+DBot.login(token);
